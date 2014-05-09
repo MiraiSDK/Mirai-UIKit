06178d347d0be4fd8a419dbf078c7c2aada4b0c2
Yonghui Chen <chyhfj@gmail.com>
2014-05-09 14:59:47 +0800

    remove unnecessary code due to Foundation framework updated

--- UIKit/UIKit+Android.h
+++ UIKit/UIKit+Android.h
@@ -13,21 +13,6 @@
 #import <UIKit/UIKitDefines.h>
 #import <CoreFoundation/CoreFoundation.h>
 
-@interface NSArray (Indexing)
-- (id)objectAtIndexedSubscript:(NSUInteger)idx;
-@end
-@interface NSMutableArray (Indexing)
-- (void)setObject:(id)obj atIndexedSubscript:(NSUInteger)idx;
-@end
-
-// NSDictionary+Indexing.h
-@interface  NSDictionary (Indexing)
-- (id)objectForKeyedSubscript:(id)key;
-@end
-@interface  NSMutableDictionary (Indexing)
-- (void)setObject:(id)obj forKeyedSubscript:(id)key;
-@end
-
 typedef NS_OPTIONS(NSUInteger, NSStringEnumerationOptions) {
     // Pass in one of the "By" options:
     NSStringEnumerationByLines = 0,                       // Equivalent to lineRangeForRange:
@@ -47,7 +32,6 @@ typedef NS_OPTIONS(NSUInteger, NSStringEnumerationOptions) {
 @end
 
 @interface NSFileManager (Android)
-- (BOOL)createDirectoryAtURL:(NSURL *)url withIntermediateDirectories:(BOOL)createIntermediates attributes:(NSDictionary *)attributes error:(NSError **)error;
 - (NSArray *)URLsForDirectory:(NSSearchPathDirectory)directory inDomains:(NSSearchPathDomainMask)domainMask;
 @end
 
@@ -58,33 +42,6 @@ typedef NS_OPTIONS(NSUInteger, NSAttributedStringEnumerationOptions) {
 @interface NSAttributedString (Android)
 - (void)enumerateAttributesInRange:(NSRange)enumerationRange options:(NSAttributedStringEnumerationOptions)opts usingBlock:(void (^)(NSDictionary *attrs, NSRange range, BOOL *stop))block;
 - (void)enumerateAttribute:(NSString *)attrName inRange:(NSRange)enumerationRange options:(NSAttributedStringEnumerationOptions)opts usingBlock:(void (^)(id value, NSRange range, BOOL *stop))block;
-
-@end
-
-enum {
-    NSSortConcurrent = (1UL << 0),
-    NSSortStable = (1UL << 4),
-};
-typedef NSUInteger NSSortOptions;
-typedef NSComparisonResult (^NSComparator)(id obj1, id obj2);
-@interface NSArray (Android)
-
-- (NSArray *)sortedArrayUsingComparator:(NSComparator)cmptr;
-- (NSArray *)sortedArrayWithOptions:(NSSortOptions)opts usingComparator:(NSComparator)cmptr;
-
-typedef NS_OPTIONS(NSUInteger, NSBinarySearchingOptions) {
-	NSBinarySearchingFirstEqual = (1UL << 8),
-	NSBinarySearchingLastEqual = (1UL << 9),
-	NSBinarySearchingInsertionIndex = (1UL << 10),
-};
-
-- (NSUInteger)indexOfObject:(id)obj inSortedRange:(NSRange)r options:(NSBinarySearchingOptions)opts usingComparator:(NSComparator)cmp;
-
-@end
-
-@interface NSDictionary (Android)
-- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL *stop))block;
-- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id key, id obj, BOOL *stop))block;
 
 @end
 
@@ -110,18 +67,11 @@ typedef NS_OPTIONS(NSUInteger, NSBinarySearchingOptions) {
 
 @end
 
-@interface NSSortDescriptor (Android)
-+ (id)sortDescriptorWithKey:(NSString *)key ascending:(BOOL)ascending;
-//+ (id)sortDescriptorWithKey:(NSString *)key ascending:(BOOL)ascending selector:(SEL)selector;
-
-@end
-
 @interface NSURL (Android)
 + (id)fileURLWithPath:(NSString *)path isDirectory:(BOOL) isDir;
 @end
 
 @interface NSIndexSet (Android)
-- (void)enumerateIndexesUsingBlock:(void (^)(NSUInteger idx, BOOL *stop))block;
 - (NSIndexSet *)indexesInRange:(NSRange)range options:(NSEnumerationOptions)opts passingTest:(BOOL (^)(NSUInteger idx, BOOL *stop))predicate;
 
 @end
@@ -133,21 +83,6 @@ typedef NS_OPTIONS(NSUInteger, NSBinarySearchingOptions) {
 @interface NSObject(UINibLoadingAdditions)
 - (void)awakeFromNib;
 @end
-
-typedef NS_OPTIONS(NSUInteger, NSDataWritingOptions) {
-    NSDataWritingAtomic = 1UL << 0,	// Hint to use auxiliary file when saving; equivalent to atomically:YES
-    NSDataWritingWithoutOverwriting = 1UL << 1, // Hint to  prevent overwriting an existing file. Cannot be combined with NSDataWritingAtomic.
-    
-    NSDataWritingFileProtectionNone = 0x10000000,
-    NSDataWritingFileProtectionComplete = 0x20000000,
-    NSDataWritingFileProtectionCompleteUnlessOpen = 0x30000000,
-    NSDataWritingFileProtectionCompleteUntilFirstUserAuthentication = 0x40000000,
-    NSDataWritingFileProtectionMask = 0xf0000000,
-    
-    // Options with old names for NSData writing methods. Please stop using these old names.
-    //NSAtomicWrite = NSDataWritingAtomic	    // Deprecated name for NSDataWritingAtomic
-};
-
 
 CFIndex CFStringGetHyphenationLocationBeforeIndex(CFStringRef string, CFIndex location, CFRange limitRange, CFOptionFlags options, CFLocaleRef locale, UTF32Char *character);
 
--- UIKit/UIKit+Android.m
+++ UIKit/UIKit+Android.m
@@ -10,34 +10,6 @@
 
 #import <CoreFoundation/CoreFoundation.h>
 
-@implementation NSArray (Indexing)
-- (id)objectAtIndexedSubscript:(NSUInteger)idx
-{
-    return [self objectAtIndex:idx];
-}
-@end
-@implementation NSMutableArray (Indexing)
-- (void)setObject:(id)obj atIndexedSubscript:(NSUInteger)idx
-{
-    [self replaceObjectAtIndex:idx withObject:obj];
-}
-@end
-
-// NSMutableDictionary+Indexing.m
-@implementation  NSDictionary (Indexing)
-
-- (id)objectForKeyedSubscript:(id)key
-{
-    return [self objectForKey:key];
-}
-@end
-@implementation  NSMutableDictionary (Indexing)
-- (void)setObject:(id)obj forKeyedSubscript:(id)key
-{
-    [self setObject:obj forKey:key];
-}
-@end
-
 @implementation NSString (Android)
 
 - (void)enumerateSubstringsInRange:(NSRange)range options:(NSStringEnumerationOptions)opts usingBlock:(void (^)(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop))block
@@ -67,12 +39,6 @@
 
 @end
 @implementation NSFileManager (Android)
-- (BOOL)createDirectoryAtURL:(NSURL *)url withIntermediateDirectories:(BOOL)createIntermediates attributes:(NSDictionary *)attributes error:(NSError **)error
-{
-    
-    return [self createDirectoryAtPath:[url path] withIntermediateDirectories:createIntermediates attributes:attributes error:error];
-}
-
 - (NSArray *)URLsForDirectory:(NSSearchPathDirectory)directory inDomains:(NSSearchPathDomainMask)domainMask
 {
     NSLog(@"%s UNIMPLEMENTED",__PRETTY_FUNCTION__);
@@ -171,57 +137,6 @@
 
 @end
 
-@implementation NSArray (Android)
-
-
-- (NSArray *)sortedArrayUsingComparator:(NSComparator)cmptr
-{
-    NSLog(@"%s unimplemented",__PRETTY_FUNCTION__);
-    return self;
-}
-
-- (NSArray *)sortedArrayWithOptions:(NSSortOptions)opts usingComparator:(NSComparator)cmptr
-{
-    NSLog(@"%s unimplemented",__PRETTY_FUNCTION__);
-    return self;
-}
-
-- (NSUInteger)indexOfObject:(id)obj inSortedRange:(NSRange)r options:(NSBinarySearchingOptions)opts usingComparator:(NSComparator)cmp
-{
-    NSLog(@"%s unimplemented",__PRETTY_FUNCTION__);
-    return NSNotFound;
-}
-
-@end
-
-@implementation NSDictionary (Android)
-- (void)enumerateKeysAndObjectsUsingBlock:(void (^)(id key, id obj, BOOL *stop))block
-{
-    [self enumerateKeysAndObjectsWithOptions:0 usingBlock:block];
-}
-
-- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^)(id key, id obj, BOOL *stop))block
-{
-    if (opts & NSEnumerationConcurrent) {
-        NSLog(@"enumerate with concurrent option is unimplemented");
-    }
-    
-    NSArray *keys = [self allKeys];
-    if (opts & NSEnumerationReverse) {
-        keys = [keys reverseObjectEnumerator].allObjects;
-    }
-    BOOL shouldStop = NO;
-    for (NSString *key in keys) {
-        block(key,self[key],&shouldStop);
-        if (shouldStop) {
-            break;
-        }
-    }
-}
-
-@end
-
-
 @interface NSBlockOperation ()
 @property (nonatomic, strong) NSMutableArray *blocks;
 @end
@@ -277,15 +192,6 @@
 
 @end
 
-@implementation NSSortDescriptor (Android)
-
-+ (id)sortDescriptorWithKey:(NSString *)key ascending:(BOOL)ascending
-{
-    return [[self alloc] initWithKey:key ascending:ascending];
-}
-
-@end
-
 @implementation NSURL (Android)
 
 + (id)fileURLWithPath:(NSString *)path isDirectory:(BOOL) isDir
@@ -296,10 +202,6 @@
 @end
 
 @implementation NSIndexSet (Android)
-- (void)enumerateIndexesUsingBlock:(void (^)(NSUInteger idx, BOOL *stop))block
-{
-    NSLog(@"%s UNIMPLEMENTED",__PRETTY_FUNCTION__);
-}
 
 - (NSIndexSet *)indexesInRange:(NSRange)range options:(NSEnumerationOptions)opts passingTest:(BOOL (^)(NSUInteger idx, BOOL *stop))predicate
 {
