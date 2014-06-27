924a9f14d16cddb5889b28a88719d087fed15568
Yonghui Chen <chyhfj@gmail.com>
2014-06-27 14:39:20 +0800

    implement textalignment convert

--- UIKit/NSText.m
+++ UIKit/NSText.m
@@ -8,3 +8,46 @@
 
 #import "NSText.h"
 
+CTTextAlignment NSTextAlignmentToCTTextAlignment(NSTextAlignment nsTextAlignment)
+{
+    CTTextAlignment ctAlignment = kCTTextAlignmentLeft;
+    
+    switch (nsTextAlignment) {
+        case NSTextAlignmentLeft:
+            ctAlignment = kCTTextAlignmentLeft;
+            break;
+        case NSTextAlignmentCenter:
+            ctAlignment = kCTTextAlignmentCenter;
+            break;
+        case NSTextAlignmentRight:
+            ctAlignment = kCTTextAlignmentRight;
+            break;
+        case NSTextAlignmentJustified:
+            ctAlignment = kCTTextAlignmentJustified;
+            break;
+        case NSTextAlignmentNatural:
+            ctAlignment = kCTTextAlignmentNatural;
+            break;
+            
+        default:
+            break;
+    }
+    return ctAlignment;
+}
+
+NSTextAlignment NSTextAlignmentFromCTTextAlignment(CTTextAlignment ctTextAlignment)
+{
+    NSTextAlignment nsAlignment = NSTextAlignmentLeft;
+    switch (ctTextAlignment) {
+        case kCTTextAlignmentLeft:nsAlignment = NSTextAlignmentLeft;break;
+        case kCTTextAlignmentCenter:nsAlignment = NSTextAlignmentCenter;break;
+        case kCTTextAlignmentRight:nsAlignment = NSTextAlignmentRight;break;
+        case kCTTextAlignmentJustified:nsAlignment = NSTextAlignmentJustified;break;
+        case kCTTextAlignmentNatural:nsAlignment = NSTextAlignmentNatural;break;
+
+        default:
+            break;
+    }
+    
+    return nsAlignment;
+}
