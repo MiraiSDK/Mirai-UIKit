14b82543a97eb939528f4f965f5bb0d6f32c657b
Yonghui Chen <chyhfj@gmail.com>
2014-08-06 15:55:31 +0800

    webview prepare

--- UIKit/UIWebView.h
+++ UIKit/UIWebView.h
@@ -1,38 +1,14 @@
 /*
- * Copyright (c) 2011, The Iconfactory. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of The Iconfactory nor the names of its contributors may
- *    be used to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE ICONFACTORY BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "UIView.h"
-#import "UIDataDetectors.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIView.h>
+#import <UIKit/UIKitDefines.h>
+#import <UIKit/UIDataDetectors.h>
+#import <UIKit/UIScrollView.h>
 
-@class UIScrollView;
 
-enum {
+typedef NS_ENUM(NSInteger, UIWebViewNavigationType) {
     UIWebViewNavigationTypeLinkClicked,
     UIWebViewNavigationTypeFormSubmitted,
     UIWebViewNavigationTypeBackForward,
@@ -40,24 +16,30 @@ enum {
     UIWebViewNavigationTypeFormResubmitted,
     UIWebViewNavigationTypeOther
 };
-typedef NSUInteger UIWebViewNavigationType;
 
-@class UIWebView, UIViewAdapter, WebView;
+typedef NS_ENUM(NSInteger, UIWebPaginationMode) {
+    UIWebPaginationModeUnpaginated,
+    UIWebPaginationModeLeftToRight,
+    UIWebPaginationModeTopToBottom,
+    UIWebPaginationModeBottomToTop,
+    UIWebPaginationModeRightToLeft
+};
 
-@protocol UIWebViewDelegate <NSObject>
-@optional
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType;
-- (void)webView:(UIWebView *)aWebView didFailLoadWithError:(NSError *)error;
-- (void)webViewDidFinishLoad:(UIWebView *)webView;
-@end
+typedef NS_ENUM(NSInteger, UIWebPaginationBreakingMode) {
+    UIWebPaginationBreakingModePage,
+    UIWebPaginationBreakingModeColumn
+};
 
-@interface UIWebView : UIView {
+@class UIWebViewInternal;
+@protocol UIWebViewDelegate;
+
+@interface UIWebView : UIView  <NSCoding, UIScrollViewDelegate> {
 @private
     __unsafe_unretained id _delegate;
     NSURLRequest *_request;
     UIDataDetectorTypes _dataDetectorTypes;
-    WebView *_webView;
-    UIViewAdapter *_webViewAdapter;
+    //WebView *_webView;
+    //UIViewAdapter *_webViewAdapter;
     BOOL _scalesPageToFit;
     
     struct {
@@ -67,24 +49,58 @@ typedef NSUInteger UIWebViewNavigationType;
     } _delegateHas;
 }
 
-- (void)loadHTMLString:(NSString *)string baseURL:(NSURL *)baseURL;
+@property(nonatomic,assign) id<UIWebViewDelegate> delegate;
+
+@property(nonatomic,readonly,retain) UIScrollView *scrollView;
+
+
 - (void)loadRequest:(NSURLRequest *)request;
-- (void)stopLoading;
+- (void)loadHTMLString:(NSString *)string baseURL:(NSURL *)baseURL;
+- (void)loadData:(NSData *)data MIMEType:(NSString *)MIMEType textEncodingName:(NSString *)textEncodingName baseURL:(NSURL *)baseURL;
+
+@property(nonatomic,readonly,retain) NSURLRequest *request;
+
 - (void)reload;
+- (void)stopLoading;
+
 - (void)goBack;
 - (void)goForward;
 
-- (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)script;
-
-@property (nonatomic, assign) id<UIWebViewDelegate> delegate;
-@property (nonatomic, readonly, getter=isLoading) BOOL loading;
 @property (nonatomic, readonly, getter=canGoBack) BOOL canGoBack;
 @property (nonatomic, readonly, getter=canGoForward) BOOL canGoForward;
-@property (nonatomic, assign) BOOL scalesPageToFit; // not implemented
-@property (nonatomic, readonly, retain) NSURLRequest *request;
+@property (nonatomic, readonly, getter=isLoading) BOOL loading;
+
+- (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)script;
+
+@property (nonatomic, assign) BOOL scalesPageToFit;
+
+@property(nonatomic) BOOL detectsPhoneNumbers; // NS_DEPRECATED_IOS(2_0, 3_0);
 @property (nonatomic) UIDataDetectorTypes dataDetectorTypes;
 
-@property(nonatomic,readonly,retain) UIScrollView *scrollView;
-@property (nonatomic) BOOL mediaPlaybackRequiresUserAction;
+@property (nonatomic) BOOL allowsInlineMediaPlayback; // iPhone Safari defaults to NO. iPad Safari defaults to YES
+@property (nonatomic) BOOL mediaPlaybackRequiresUserAction; //default to YES
+@property (nonatomic) BOOL mediaPlaybackAllowsAirPlay; // iPhone and iPad Safari both default to YES
+
+@property (nonatomic) BOOL suppressesIncrementalRendering; // iPhone and iPad Safari both default to NO
+
+@property (nonatomic) BOOL keyboardDisplayRequiresUserAction; // default is YES
+
+@property (nonatomic) UIWebPaginationMode paginationMode; //NS_AVAILABLE_IOS(7_0);
+@property (nonatomic) UIWebPaginationBreakingMode paginationBreakingMode; //NS_AVAILABLE_IOS(7_0);
+@property (nonatomic) CGFloat pageLength; //NS_AVAILABLE_IOS(7_0);
+@property (nonatomic) CGFloat gapBetweenPages; //NS_AVAILABLE_IOS(7_0);
+@property (nonatomic, readonly) NSUInteger pageCount; //NS_AVAILABLE_IOS(7_0);
 
 @end
+
+@protocol UIWebViewDelegate <NSObject>
+
+@optional
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType;
+- (void)webViewDidStartLoad:(UIWebView *)webView;
+- (void)webViewDidFinishLoad:(UIWebView *)webView;
+- (void)webView:(UIWebView *)aWebView didFailLoadWithError:(NSError *)error;
+
+@end
+
+
