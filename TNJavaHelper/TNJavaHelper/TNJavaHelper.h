9b50f697bbfa0310843e543f54e11427806f754b
Jack <jack@tiny4cocoa.org>
2014-08-10 22:48:05 +0800

    add JavaHelper project

---
+++ TNJavaHelper/TNJavaHelper/TNJavaHelper.h
@@ -0,0 +1,19 @@
+//
+//  TNJavaHelper.h
+//  TNJavaHelper
+//
+//  Created by Chen Yonghui on 7/25/14.
+//  Copyright (c) 2014 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include <jni.h>
+
+@interface TNJavaHelper : NSObject
++ (void)initializeWithVM:(JavaVM *)vm activityClass:(jclass)clazz;
++ (instancetype)sharedHelper;
+- (JNIEnv *)env;
+- (jclass)findCustomClass:(NSString *)className;
+- (JavaVM *)vm;
+
+@end
