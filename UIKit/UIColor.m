507f9a35fb9df423e1ed51556ef4a51d8f9dd3eb
Yonghui Chen <chyhfj@gmail.com>
2014-08-28 16:30:46 +0800

    fix hsv to RGB convert

--- UIKit/UIColor.m
+++ UIKit/UIColor.m
@@ -85,12 +85,64 @@ static void rgbaFromHSL(CGFloat hue, CGFloat saturation, CGFloat brightness, CGF
     *outAlpha = alpha;
 }
 
+void HSVtoRGB(float h, float s, float v,
+              float *r, float *g, float *b)
+{
+    int i;
+	float f, p, q, t;
+	if( s == 0 ) {
+		// achromatic (grey)
+		*r = *g = *b = v;
+		return;
+	}
+	h /= 60;			// sector 0 to 5
+	i = floor( h );
+	f = h - i;			// factorial part of h
+    i %= 6;             //
+	p = v * ( 1 - s );
+	q = v * ( 1 - s * f );
+	t = v * ( 1 - s * ( 1 - f ) );
+	switch( i ) {
+		case 0:
+			*r = v;
+			*g = t;
+			*b = p;
+			break;
+		case 1:
+			*r = q;
+			*g = v;
+			*b = p;
+			break;
+		case 2:
+			*r = p;
+			*g = v;
+			*b = t;
+			break;
+		case 3:
+			*r = p;
+			*g = q;
+			*b = v;
+			break;
+		case 4:
+			*r = t;
+			*g = p;
+			*b = v;
+			break;
+		default:		// case 5:
+			*r = v;
+			*g = p;
+			*b = q;
+			break;
+	}
+}
+
 - (UIColor *)initWithHue:(CGFloat)hue saturation:(CGFloat)saturation brightness:(CGFloat)brightness alpha:(CGFloat)alpha
 {
-    CGFloat red,green,blue,a;
-    rgbaFromHSL(hue, saturation, brightness, alpha,&red,&green,&blue,&a);
+    CGFloat red,green,blue;
+    CGFloat h = hue * 360.0f;
     
-    return [self initWithRed:red green:green blue:blue alpha:a];
+    HSVtoRGB(h, saturation, brightness, &red, &green, &blue);
+    return [self initWithRed:red green:green blue:blue alpha:alpha];
 }
 
 - (UIColor *)initWithRed:(CGFloat)red green:(CGFloat)green blue:(CGFloat)blue alpha:(CGFloat)alpha
