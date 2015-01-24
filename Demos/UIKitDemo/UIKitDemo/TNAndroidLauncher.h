11a4eef0b210bebb7af7dab37bfe8df921e19b41
Yonghui Chen <chyhfj@gmail.com>
2015-01-25 00:10:02 +0800

    add a Demo Project

---
+++ Demos/UIKitDemo/UIKitDemo/TNAndroidLauncher.h
@@ -0,0 +1,13 @@
+//
+//  TNAndroidLauncher.h
+//  UIKitDemo
+//
+//  Created by Chen Yonghui on 1/24/15.
+//  Copyright (c) 2015 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TNAndroidLauncher : NSObject
+
+@end
---
+++ Demos/UIKitDemo/UIKitDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  UIKitDemo
+//
+//  Created by Chen Yonghui on 1/24/15.
+//  Copyright (c) 2015 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
