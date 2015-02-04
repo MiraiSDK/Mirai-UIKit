c0891e4f58b4c99193da763d3d1f11be372ddae4
moskize <xiangtan_tao@126.com>
2015-02-04 23:40:19 +0800

    implement a simple UISwitch

--- UIKit/UISwitch.m
+++ UIKit/UISwitch.m
@@ -18,15 +18,33 @@
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:CGRectMake(frame.origin.x, frame.origin.y, 51, 31)];
+    self = [super initWithFrame:frame];
     if (self) {
+        [self addTarget:self
+                 action:@selector(_clickSwitch:)
+       forControlEvents:UIControlEventTouchUpInside];
     }
     return self;
 }
 
+- (void)_clickSwitch:(id)sender
+{
+    [self setOn:!_on animated:YES];
+}
+
 - (void)setOn:(BOOL)on animated:(BOOL)animated
 {
+    BOOL changed = (_on != on);
     _on = on;
+    
+    if (changed) {
+        [self _dispatchValueChanged];
+    }
+}
+
+- (void)_dispatchValueChanged
+{
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
 }
 
 - (void)encodeWithCoder:(NSCoder *)aCoder
