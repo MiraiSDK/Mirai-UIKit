0cb2e2819a98bbe1a3adf3db9a6363209f0d994c
moskize <xiangtan_tao@126.com>
2015-02-08 16:30:38 +0800

    use subviews to implement appearance.

--- UIKit/UISwitch.m
+++ UIKit/UISwitch.m
@@ -5,22 +5,22 @@
 //  Created by Chen Yonghui on 10/20/14.
 //  Copyright (c) 2014 Shanghai Tinynetwork Inc. All rights reserved.
 //
+#import <QuartzCore/QuartzCore.h>
 
 #import "UISwitch.h"
 #import "NSStringDrawing.h"
-#import "UIGraphics.h"
 #import "UILabel.h"
 
-#define ButtonAnimationNeedFrames 6
 #define LineWith 2.0
-
-
+#define ButtonMoveTime 0.7
+#define LineColor CGColorCreateGenericRGB(0.5, 0.5, 0.5, 1)
+#define ButtonColor [UIColor colorWithRed:1 green:1 blue:1 alpha:1]
+#define OnBackgroudColor [UIColor colorWithRed:0 green:0.7 blue:0 alpha:1]
+#define OffBackgroudColor [UIColor colorWithRed:0.5 green:0.7 blue:0.5 alpha:1]
 
 @interface UISwitch()
-@property CGFloat buttonLocation;
-@property NSInteger currentFrameCount;
-@property BOOL isPlayingAnimation;
-@property (nonatomic, strong) NSTimer *timer;
+@property UIView *backgroudView;
+@property UIView *buttonView;
 @end
 
 @implementation UISwitch
@@ -30,121 +30,15 @@
     return NO;
 }
 
-+ (CGColorRef)_OnBackgroudColor
-{
-    static CGColorRef color;
-    if (color == nil) {
-        color = CGColorCreateGenericRGB(0.2, 0.7, 0.2, 1);
-    }
-    return color;
-}
-
-+ (CGColorRef)_OffBackgroudColor
-{
-    static CGColorRef color;
-    if (color == nil) {
-        color = CGColorCreateGenericRGB(0, 1, 0, 1);
-    }
-    return color;
-}
-
-+ (CGColorRef)_LineColor
-{
-    static CGColorRef color;
-    if (color == nil) {
-        color = CGColorCreateGenericRGB(0.5, 0.5, 0.5, 1);
-    }
-    return color;
-}
-
-+ (CGColorRef)_ButtonColor
-{
-    static CGColorRef color;
-    if (color == nil) {
-        color = CGColorCreateGenericRGB(1, 1, 1, 1);
-    }
-    return color;
-}
-
 - (instancetype)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        [self addTarget:self
-                 action:@selector(_clickSwitch:)
-       forControlEvents:UIControlEventTouchUpInside];
+        [self _registerTouchUpEvent];
+        [self _makeSubviews];
+        [self _setViewStateWithOn:NO];
     }
     return self;
-}
-
-- (void)_startFramesAnimation
-{
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.015
-                                                  target:self
-                                                selector:@selector(_moveButtonEnterFrame:)
-                                                userInfo:nil
-                                                 repeats:YES];
-}
-
-- (void)_stopFramesAnimation
-{
-    if (self.timer != nil){
-        [self.timer invalidate];
-        self.timer = nil;
-    }
-}
-
-- (void)_clickSwitch:(id)sender
-{
-    [self setOn:!_on animated:YES];
-}
-
-- (void)setOn:(BOOL)on animated:(BOOL)animated
-{
-    BOOL changed = (_on != on);
-    _on = on;
-    
-    if (changed) {
-        [self _dispatchValueChanged];
-        if(animated) {
-            [self _playMoveButtonAnimation];
-        } else {
-            self.buttonLocation = _on? 1.0: 0.0;
-        }
-    }
-}
-
-- (void)_dispatchValueChanged
-{
-    [self sendActionsForControlEvents:UIControlEventValueChanged];
-}
-
-- (void)_playMoveButtonAnimation
-{
-    self.isPlayingAnimation = YES;
-    self.currentFrameCount = YES;
-    [self _startFramesAnimation];
-}
-
-- (void) _moveButtonEnterFrame:(NSTimer *)paramTimer
-{
-    if(_on)
-    {
-        self.buttonLocation = (CGFloat)self.currentFrameCount / (CGFloat)ButtonAnimationNeedFrames;
-    }
-    else
-    {
-        self.buttonLocation = 1.0 - (CGFloat)self.currentFrameCount / (CGFloat)ButtonAnimationNeedFrames;
-    }
-    self.buttonLocation *= self.buttonLocation; //let animation looks more smooth.
-    
-    if(self.currentFrameCount >= ButtonAnimationNeedFrames) {
-        [self _stopFramesAnimation];
-        self.isPlayingAnimation = NO;
-    } else {
-        self.currentFrameCount++;
-    }
-    [self setNeedsDisplay];
 }
 
 - (void)encodeWithCoder:(NSCoder *)aCoder
@@ -157,90 +51,96 @@
     return self;
 }
 
-- (void) drawRect:(CGRect)rect
+- (void)_registerTouchUpEvent
 {
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSaveGState(context);
-    
-    CGRect drawArea = CGRectInset(rect, LineWith, LineWith);
-    [self _drawBackground:drawArea withContextRef:context];
-    [self _drawButton:drawArea withContextRef:context];
-    
-    CGContextRestoreGState(context);
+    [self addTarget:self
+             action:@selector(_clickSwitch:)
+   forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)_drawBackground:(CGRect)rect withContextRef:(CGContextRef) context
+- (void)_clickSwitch:(id)sender
 {
-    CGContextSetStrokeColorWithColor(context, [UISwitch _LineColor]);
-    CGContextSetFillColorWithColor(context, [self _getCurrentBackgroundColor]);
-    CGContextSetLineWidth(context, LineWith);
-    
-    [self _drawCapsuleRect:rect withContextRef:context];
-    
-    CGContextDrawPath(context, kCGPathFillStroke);
+    [self setOn:!_on animated:YES];
 }
 
-- (CGColorRef)_getCurrentBackgroundColor
+- (void)_makeSubviews
 {
-    if (self.isPlayingAnimation) {
-        return _on? [UISwitch _OffBackgroudColor]: [UISwitch _OnBackgroudColor];
+    [self _makeBackgroudView];
+    [self _makeButtonView];
+}
+
+- (void)_makeBackgroudView
+{
+    CGFloat radius = self.bounds.size.height;
+    
+    self.backgroudView = [[UIView alloc] initWithFrame:self.bounds];
+    self.backgroudView.layer.cornerRadius = radius;
+    self.backgroudView.layer.masksToBounds = YES;
+    self.backgroudView.layer.borderWidth = LineWith;
+    self.backgroudView.layer.borderColor = LineColor;
+    
+    [self addSubview:self.backgroudView];
+}
+
+- (void)_makeButtonView
+{
+    CGFloat radius = self.bounds.size.height;
+    CGRect buttonRect = CGRectMake(self.bounds.origin.x, self.bounds.origin.y, self.bounds.size.height, self.bounds.size.height);
+    
+    self.buttonView = [[UIView alloc] initWithFrame:buttonRect];
+    self.buttonView.layer.cornerRadius = radius;
+    self.buttonView.layer.masksToBounds = YES;
+    self.buttonView.layer.borderWidth = LineWith;
+    self.buttonView.layer.borderColor = LineColor;
+    self.buttonView.backgroundColor = ButtonColor;
+    
+    [self addSubview:self.buttonView];
+}
+
+- (void)setOn:(BOOL)on animated:(BOOL)animated
+{
+    BOOL changed = (_on != on);
+    _on = on;
+    
+    if (changed) {
+        [self _dispatchValueChanged];
+        if(animated) {
+            [self _playMoveButtonAnimationWithOn:on];
+        } else {
+            [self _setViewStateWithOn:on];
+        }
+    }
+}
+
+- (void)_dispatchValueChanged
+{
+    [self sendActionsForControlEvents:UIControlEventValueChanged];
+}
+
+- (void)_playMoveButtonAnimationWithOn:(BOOL)on
+{
+    [UIView animateWithDuration:ButtonMoveTime animations:^{
+        [self _setViewStateWithOn:on];
+    }];
+}
+
+- (void)_setViewStateWithOn:(BOOL)on
+{
+    CGFloat radius = self.bounds.size.height;
+    if(on) {
+        self.backgroudView.backgroundColor = OnBackgroudColor;
+        self.buttonView.frame = [self _crateChangedOriginRectFrom:self.buttonView.frame
+                                                            withX:(self.bounds.size.width - radius)];
     } else {
-        return _on? [UISwitch _OnBackgroudColor]: [UISwitch _OffBackgroudColor];
+        self.backgroudView.backgroundColor = OffBackgroudColor;
+        self.buttonView.frame = [self _crateChangedOriginRectFrom:self.buttonView.frame
+                                                            withX:0];
     }
 }
 
--(void) _drawButton:(CGRect)moveArea withContextRef:(CGContextRef)context
+- (CGRect)_crateChangedOriginRectFrom:(CGRect)oldRect withX:(CGFloat)x
 {
-    CGFloat radius = moveArea.size.height;
-    CGFloat buttonLeft = moveArea.origin.x + ( moveArea.size.width - radius )*self.buttonLocation;
-    CGFloat buttonTop = moveArea.origin.y;
-    
-    CGContextSetStrokeColorWithColor(context, [UISwitch _LineColor]);
-    CGContextSetFillColorWithColor(context, [UISwitch _ButtonColor]);
-    CGContextSetLineWidth(context, LineWith);
-    
-    [self _drawCircleInRect:CGRectMake(buttonLeft, buttonTop, radius, radius) withContextRef:context];
-    
-    CGContextDrawPath(context, kCGPathFillStroke);
-}
-
--(void) _drawCircleInRect:(CGRect)rect withContextRef:(CGContextRef) context
-{
-    if(rect.size.width != rect.size.height) {
-        rect.size.height = rect.size.width;
-    }
-    CGFloat x0 = rect.origin.x;
-    CGFloat y0 = rect.origin.y;
-    CGFloat x1 = rect.origin.x + rect.size.width;
-    CGFloat y1 = rect.origin.y + rect.size.height;
-    
-    CGFloat radius = rect.size.width/2;
-    
-    CGContextMoveToPoint(context, x0, y0 + radius);
-    CGContextAddArcToPoint(context, x0, y0, x0+radius, y0, radius);
-    CGContextAddArcToPoint(context, x1, y0, x1, y0 + radius, radius);
-    CGContextAddArcToPoint(context, x1, y1, x1 - radius, y1, radius);
-    CGContextAddArcToPoint(context, x0, y1, x0, y1 - radius, radius);
-    CGContextClosePath(context);
-}
-
-- (void) _drawCapsuleRect:(CGRect)rect withContextRef:(CGContextRef) context
-{
-    CGFloat x0 = rect.origin.x;
-    CGFloat y0 = rect.origin.y;
-    CGFloat x1 = rect.origin.x + rect.size.width;
-    CGFloat y1 = rect.origin.y + rect.size.height;
-    
-    CGFloat radius = rect.size.height/2;
-    
-    CGContextMoveToPoint(context, x0, y0 + radius);
-    CGContextAddArcToPoint(context, x0, y0, x0+radius, y0, radius);
-    CGContextAddLineToPoint(context, x1 - radius, y0);
-    CGContextAddArcToPoint(context, x1, y0, x1, y0 + radius, radius);
-    CGContextAddArcToPoint(context, x1, y1, x1 - radius, y1, radius);
-    CGContextAddLineToPoint(context, x0 + radius, y1);
-    CGContextAddArcToPoint(context, x0, y1, x0, y1 - radius, radius);
-    CGContextClosePath(context);
+    return CGRectMake(x, oldRect.origin.y, oldRect.size.width, oldRect.size.height);
 }
 
 @end
