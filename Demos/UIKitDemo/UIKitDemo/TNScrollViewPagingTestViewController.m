fdf52563390c84ca882088532a567fdbf8eda21d
Yonghui Chen <chyhfj@gmail.com>
2015-03-18 13:23:26 +0800

    improve scrollview test

--- Demos/UIKitDemo/UIKitDemo/TNScrollViewPagingTestViewController.m
+++ Demos/UIKitDemo/UIKitDemo/TNScrollViewPagingTestViewController.m
@@ -9,10 +9,18 @@
 #import "TNScrollViewPagingTestViewController.h"
 
 @interface TNScrollViewPagingTestViewController () <UIScrollViewDelegate>
-
+@property (nonatomic, strong) UIScrollView *scrollView;
+@property (nonatomic, assign) NSInteger pageCount;
+@property (nonatomic, strong) NSArray *contents;
+@property (nonatomic, assign,getter=isVertical) BOOL vertical;
 @end
 
 @implementation TNScrollViewPagingTestViewController
+
+- (void)dealloc
+{
+    _scrollView.delegate = nil;
+}
 
 + (NSString *)testName
 {
@@ -23,27 +31,75 @@
 {
     [super viewDidLoad];
     
-    NSInteger pageCount = 8;
-    CGFloat width = CGRectGetWidth(self.view.bounds);
-    CGFloat height = CGRectGetHeight(self.view.bounds);
+    self.pageCount = 8;
     
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.contentSize = CGSizeMake(width * pageCount, height);
+    scrollView.contentSize = [self contentSizeForViewSize:self.view.bounds.size];
     scrollView.pagingEnabled = YES;
     scrollView.delegate = self;
     
-    scrollView.layer.borderWidth = 100;
+    scrollView.layer.borderWidth = 2;
     scrollView.layer.borderColor = [UIColor redColor].CGColor;
+    self.scrollView = scrollView;
 
     [self.view addSubview:scrollView];
     
-    for (int idx=0; idx<pageCount; idx++) {
-        CGRect r = CGRectMake(width * idx, 0, width, height);
-        UIView *view = [[UIView alloc] initWithFrame:r];
-        CGFloat h = idx/(float)pageCount;
-        view.backgroundColor = [UIColor colorWithHue:h saturation:1 brightness:1 alpha:1];
+    NSMutableArray *contents = [NSMutableArray array];
+    for (int idx=0; idx<self.pageCount; idx++) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectZero];
+        [contents addObject:view];
         [scrollView addSubview:view];
     }
+    self.contents = contents;
+    
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"direction" style:UIBarButtonItemStylePlain target:self action:@selector(changeDirection:)];
+    self.navigationItem.rightBarButtonItems = @[item];
+    
+    [self configureScrollViewContents];
+    
+}
+
+- (void)changeDirection:(id)sender
+{
+    self.vertical = !self.isVertical;
+    
+    [self configureScrollViewContents];
+}
+
+- (CGSize)contentSizeForViewSize:(CGSize)size
+{
+    if (self.isVertical) {
+        return CGSizeMake(size.width, size.height * self.pageCount);
+    }
+    return CGSizeMake(size.width * self.pageCount, size.height);
+}
+
+- (void)configureScrollViewContents
+{
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat height = CGRectGetHeight(self.view.bounds);
+    
+    self.scrollView.frame = self.view.bounds;
+    self.scrollView.contentSize = [self contentSizeForViewSize:self.view.bounds.size];
+
+    for (int idx = 0; idx < self.pageCount; idx++) {
+        UIView *view = self.contents[idx];
+        CGRect r;
+        if (self.isVertical) {
+            r = CGRectMake(0 , height * idx, width, height);
+        } else {
+            r = CGRectMake(width * idx , 0, width, height);
+        }
+        view.frame = r;
+        CGFloat h = idx/(float)self.pageCount;
+        view.backgroundColor = [UIColor colorWithHue:h saturation:1 brightness:1 alpha:1];
+    }
+    
+}
+
+- (void)viewWillLayoutSubviews
+{
+    [self configureScrollViewContents];
 }
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView
