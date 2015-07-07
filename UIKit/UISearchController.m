2acd2d3c069e1e5c54365bd8755a30d1db3a407d
moskize <xiangtan_tao@126.com>
2015-07-07 17:53:37 +0800

    improve a simple UISearchController.
    a simple UISearchController can only show resultsViewController’s view.

--- UIKit/UISearchController.m
+++ UIKit/UISearchController.m
@@ -8,36 +8,125 @@
 
 #import "UISearchController.h"
 
-@interface UISearchController ()
-
+@interface _UISearchResultsViewContainer : UIView
+@property (nonatomic) BOOL dimsBackgroundDuringPresentation;
+- (instancetype)initWithSearchController:(UISearchController *)searchController;
 @end
 
+static void SetViewAutoresizeToMatchSuperview(UIView *view) {
+    view.frame = [UIScreen mainScreen].bounds; // delete this line after test.
+    view.autoresizingMask = UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleLeftMargin |
+                            UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleRightMargin |
+                            UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+}
+
 @implementation UISearchController
+{
+    _UISearchResultsViewContainer *_searchResultsViewContainer;
+    UIWindow *_searchResultsContainerWindow;
+}
 
 - (instancetype)initWithSearchResultsController:(UIViewController *)searchResultsController
 {
-    self = [super init];
+    if (self = [super init]) {
+        [self _settingDefaultValues];
+        
+        _searchResultsController = searchResultsController;
+        _searchResultsViewContainer = [[_UISearchResultsViewContainer alloc] initWithSearchController:self];
+        _searchResultsContainerWindow = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+        
+        [_searchResultsContainerWindow setRootViewController:self];
+    }
     return self;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)loadView
+{
+    [super loadView];
+    UIView *view = self.view;
+    
+    [view addSubview:_searchResultsViewContainer];
+    view.backgroundColor = [UIColor redColor]; // delte it after test.
+    SetViewAutoresizeToMatchSuperview(view);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)_settingDefaultValues
+{
+    _active = NO;
+    _dimsBackgroundDuringPresentation = YES;
+    _hidesNavigationBarDuringPresentation = YES;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)setDimsBackgroundDuringPresentation:(BOOL)dimsBackgroundDuringPresentation
+{
+    _dimsBackgroundDuringPresentation = dimsBackgroundDuringPresentation;
+    _searchResultsViewContainer.dimsBackgroundDuringPresentation = dimsBackgroundDuringPresentation;
 }
-*/
+
+- (void)setActive:(BOOL)active
+{
+    if (_active == active) {
+        return;
+    }
+    _active = active;
+    _searchResultsContainerWindow.hidden = !active;
+    
+    NSLog(@"-> %@", NSStringFromCGRect(_searchResultsViewContainer.frame));
+}
+
+@end
+
+@implementation _UISearchResultsViewContainer
+{
+    UISearchController *_searchController;
+}
+
+- (instancetype)initWithSearchController:(UISearchController *)searchController
+{
+    if (self = [super init]) {
+        _searchController = searchController;
+        SetViewAutoresizeToMatchSuperview(self);
+        [self setDimsBackgroundDuringPresentation:searchController.dimsBackgroundDuringPresentation];
+        [self _registerTappedEvent];
+    }
+    return self;
+}
+
+- (void)setDimsBackgroundDuringPresentation:(BOOL)dimsBackgroundDuringPresentation
+{
+    if (_dimsBackgroundDuringPresentation == dimsBackgroundDuringPresentation) {
+        return;
+    }
+    _dimsBackgroundDuringPresentation = dimsBackgroundDuringPresentation;
+    
+    if (dimsBackgroundDuringPresentation) {
+        [self setBackgroundColor:[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.5]];
+    } else {
+        [self setBackgroundColor:[UIColor clearColor]];
+    }
+}
+
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
+{
+    UIView *hitView = [super hitTest:point withEvent:event];
+    
+    if (_dimsBackgroundDuringPresentation) {
+        return hitView;
+    } else {
+        // can't hit this container self.
+        return hitView == self? nil: hitView;
+    }
+}
+
+- (void)_registerTappedEvent
+{
+    UITapGestureRecognizer *singletapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(_onTappedSelf:)];
+    [self addGestureRecognizer:singletapGestureRecognizer];
+}
+
+- (void)_onTappedSelf:(id)sender
+{
+    [_searchController setActive:NO];
+}
 
 @end
