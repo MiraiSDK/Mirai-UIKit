ea6694202e3de8fd41a0d895acaa40c17df0b0c5
moskize <xiangtantaozeyu@icloud.com>
2015-10-03 21:55:11 +0800

    add UITouch.tapCount test code.

---
+++ Demos/UIKitDemo/UIKitDemo/TNMultiTouchTapCountTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNMultiTouchTapCountTestViewController.h
+//  UIKitDemo
+//
+//  Created by TaoZeyu on 15/10/3.
+//  Copyright © 2015年 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import "TNTestViewController.h"
+
+@interface TNMultiTouchTapCountTestViewController : TNTestViewController
+
+@end
